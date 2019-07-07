afba191a729117bdd552fd39aaca11fadcd44b50
liubaoquan <liubaoquan@192.168.1.110>
2019-07-07 12:40:33 +0800

    add log add use software

--- netty-chat/src/main/java/sample/netty/chat/handler/codec/SampleChatDecoder.java
+++ netty-chat/src/main/java/sample/netty/chat/handler/codec/SampleChatDecoder.java
@@ -3,6 +3,9 @@ package sample.netty.chat.handler.codec;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
@@ -19,6 +22,8 @@ import sample.netty.chat.handler.SampleChatInfo;
  */
 public class SampleChatDecoder extends ByteToMessageDecoder {
 
+    private static final Logger logger = LoggerFactory.getLogger(SampleChatDecoder.class);
+
     private static final byte[] HEADER = { 0x75, 0x72 }; // 报文头
 
     private static final int MIN_LENGTH = 2 + 2; // 报文头 + 数据长度
@@ -29,12 +34,13 @@ public class SampleChatDecoder extends ByteToMessageDecoder {
 
         // 数据未全部到达
         if (in.readableBytes() < MIN_LENGTH) {
+            logger.debug("数据未全部到达......");
             return;
-
         }
 
         // 数据包太大
         if (in.readableBytes() >= MAX_LENGTH) {
+            logger.warn("数据包太大,数据包大小:{}", in.readableBytes());
             in.skipBytes(in.readableBytes());
             return;
         }
@@ -63,6 +69,7 @@ public class SampleChatDecoder extends ByteToMessageDecoder {
 
             // 当略过，一个字节之后，数据包的长度不足,等待后续数据
             if (in.readableBytes() < MIN_LENGTH) {
+                logger.warn("非完整的数据包");
                 return;
             }
 
@@ -74,6 +81,7 @@ public class SampleChatDecoder extends ByteToMessageDecoder {
         // 读取数据
         // 判断请求数据包数据是否到齐
         if (in.readableBytes() < length) {
+            logger.info("数据未全部到达,等待数据到达");
             // 还原读指针
             in.readerIndex(beginReader);
             return;
